8dde4f01ecea861493c341980c08b94a04a65018
JorTurFer <jorge_turrado@hotmail.es>
2019-02-23 15:52:31 +0100

    renombro clase, añado funcionalidad y descripcion basica del paquete

---
+++ NugetColaboraciónVNF/ExampleClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NugetColaboraciónVNF
+{
+    public static class ExampleClass
+    {
+        public static string GetString()
+        {
+            return "Paquete de prueba 'VariableNotFound && FixedBuffer'";
+        }
+    }
+}
